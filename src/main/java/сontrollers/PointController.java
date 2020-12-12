4ac232b3dc9e3fd46919f0e2cf8dd87e5a901c9b
Ali.Alibekovich <a.alibekovich@yandex.ru>
2020-12-12 04:20:03 +0300

    Update PointController.java

--- src/main/java/сontrollers/PointController.java
+++ src/main/java/сontrollers/PointController.java
@@ -12,11 +12,5 @@ import javax.ws.rs.core.Response;
 @Path("users")
 @Consumes(MediaType.APPLICATION_JSON)
 public class PointController {
-    @POST
-    public Response insertUser(User user) {
-        System.out.println(user.getLogin());
-        System.out.println(user.getPassword());
-        return Response.ok("Вы успешно зарегистрированы").build();
-    }
 
 }
