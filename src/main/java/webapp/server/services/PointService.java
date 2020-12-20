19935ae8d587ea91bf3c4e3c640125b7154f9f5a
antondzubak <nesladkiy@icloud.com>
2020-12-21 01:12:28 +0300

    User-check fixed 3

--- src/main/java/webapp/server/services/PointService.java
+++ src/main/java/webapp/server/services/PointService.java
@@ -6,7 +6,10 @@ import webapp.server.db.entities.PointEntity;
 
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
+import javax.persistence.Query;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Singleton
 public class PointService {
@@ -24,12 +27,13 @@ public class PointService {
         Добавляет объект в базу данных.
      */
     public void addEntity(double x, double y, double r, String owner) {
+        updatePointsList();
         PointEntity entity = addEntityInDB(x, y, r, owner);
         addPointInArray(x, y, r, owner, entity);
     }
 
     public ArrayList<Point> getUserPoints(String owner) {
-        return points;
+        return points.stream().filter(o -> o.getOwner().equals(owner)).collect(Collectors.toCollection(ArrayList::new));
     }
 
 
@@ -52,5 +56,20 @@ public class PointService {
         point.setOwner(owner);
         point.setHit(entity.getHit());
         points.add(point);
+    }
+
+    public void updatePointsList(){
+        Query query = connector.getEntityManager().createQuery("SELECT entity FROM PointEntity entity");
+        List<PointEntity> pointEntityList = query.getResultList();
+        points.clear();
+        for (PointEntity pointEntity : pointEntityList) {
+            Point point = new Point();
+            point.setX(pointEntity.getX());
+            point.setY(pointEntity.getY());
+            point.setR(pointEntity.getR());
+            point.setOwner(pointEntity.getOwner());
+            point.setHit(pointEntity.getHit());
+            points.add(point);
+        }
     }
 }
